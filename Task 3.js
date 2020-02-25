a83b67d78e901443fa8ab2c61290489460451721
Tolik Khadasevich <tolikxx2000@gmail.com>
2020-02-25 03:48:36 +0300

    Added homework for lesson 2

--- Task 3.js
+++
@@ -1,8 +0,0 @@
-const a = Number(prompt("Введите число a: "));
-const b = Number(prompt("Введите число b: "));
-
-if (a % b === 0) {
-    alert("Число a делится нацело на число b");
-} else {
-    alert("Число a НЕ делится нацело на число b");
-}
